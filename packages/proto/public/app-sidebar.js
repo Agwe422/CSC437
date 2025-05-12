563fbccae4916a45fefb7f2660960b76880537d4
Josh Hall <113940171+Agwe422@users.noreply.github.com>
2025-05-11 20:05:20 -0700

    lab 8 vite not working

---
+++ packages/proto/public/app-sidebar.js
@@ -0,0 +1,47 @@
+import { LitElement, html, css } from 'https://unpkg.com/lit?module';
+import { define } from 'https://unpkg.com/@calpoly/mustang?module';
+
+class AppSidebarElement extends LitElement {
+  static styles = css`
+    .sidebar {
+      position: fixed;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      width: var(--sidebar-width);
+      background-color: var(--color-background-header);
+      padding: var(--spacing-md);
+    }
+    .sidebar a {
+      display: flex;
+      align-items: center;
+      gap: var(--spacing-xxs);
+      color: var(--color-link);
+      margin-bottom: var(--spacing-md);
+      text-decoration: none;
+    }
+    svg.icon {
+      width: var(--icon-size);
+      height: var(--icon-size);
+      fill: currentColor;
+    }
+  `;
+
+  render() {
+    return html`
+      <aside class="sidebar">
+        <a href="/index.html">
+          Home
+        </a>
+        <a href="/playlist1.html">
+          <svg class = "icon">
+          <use xlink:href="/icons/app-icons.svg#playlist" />
+        </svg>
+          Playlist 1
+        </a>
+      </aside>
+    `;
+  }
+}
+
+define({ 'app-sidebar': AppSidebarElement });
---
+++ packages/proto/vite.config.js
@@ -0,0 +1,16 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        playlist1: resolve(__dirname, 'playlist1.html'),
+      }
+    }
+  }
+});
